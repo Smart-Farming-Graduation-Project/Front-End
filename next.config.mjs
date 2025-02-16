de729a7c4e5055de4f9e5292775974cdf1c7a196
Mohamed Elsayed <86891363+Mohamedzonkol@users.noreply.github.com>
2025-02-16 18:18:23 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build',
-  output: 'standalone',
+  output: 'standalone', 
+  outputFileTracing: true, 
 };
 
 export default nextConfig;
